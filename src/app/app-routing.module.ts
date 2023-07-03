f7b4d38e2530dc8a8178daa5a6a822f119ccb03f
Jorge Arias <jorgearias.ja@gmail.com>
2023-07-03 03:10:54 -0500

    added api connection and signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, provideRouter, withComponentInputBinding } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { PasswordListComponent } from './components/password-list/password-list.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'password-list', component: PasswordListComponent},
+  {path: 'sign-up', component: SignUpComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    provideRouter(routes, withComponentInputBinding())
+  ]
 })
 export class AppRoutingModule { }
---
+++ src/app/components/sign-up/sign-up.component.ts
@@ -0,0 +1,40 @@
+import { FormControl, FormGroup } from '@angular/forms';
+import { Component } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { ICreateUser } from 'src/app/models/ICreateUser';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent {
+
+  signUpForm = new FormGroup({
+    firstname: new FormControl(''),
+    lastname: new FormControl(''),
+    phoneNumber: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  constructor(private apiService: ApiService) {}
+
+  onSubmit() {
+    let user: ICreateUser = {
+      firstname: this.signUpForm.controls.firstname.value || '',
+      lastname: this.signUpForm.controls.lastname.value || '',
+      phoneNumber: parseInt(this.signUpForm.controls.phoneNumber.value || ''),
+      email: this.signUpForm.controls.email.value || '',
+      password: this.signUpForm.controls.password.value || ''
+    }
+
+    this.apiService.createUser(user)
+      .subscribe(res => {
+        console.log('CreateUser: ' , res);
+      }, err => {
+        console.error('Error: ' , err);
+      })
+  }
+
+}
